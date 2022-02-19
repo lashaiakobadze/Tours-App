8849f9e7b9ff01b5f6e5be6fece75f54241d73ae
Lasha <lasha.iakobadze@singular.uk>
2022-02-19 23:23:10 +0400

    Fix raw parser

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const compression = require('compression');
 const cors = require('cors');
 
@@ -153,7 +154,7 @@ app.use('/api', limiter);
 // Stripe specific security system.
 app.post(
   '/webhooks-checkout',
-  express.raw({ type: 'application.json' }),
+  bodyParser.raw({ type: 'application.json' }),
   bookingController.webhooksCheckout
 );
 
